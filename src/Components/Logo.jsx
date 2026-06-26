c66bb6b44618bf3f395b83b680d2d5983f216dbd
Mauzzam Shaikh <ms5880188@gmail.com>
2026-06-26 21:49:58 +0530

    Updated Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/Logo.png";
 function Logo({ width = "180px" }) {
   return (
     <img
-      src={Logo}
+      src={logo}
       alt="My Blog"
       style={{ width }}
       className="object-contain"
